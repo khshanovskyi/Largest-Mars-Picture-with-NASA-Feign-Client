d5581392f964c1a49b2cc1b1bfdd4952dc17503f
khshanovskyi <khshanovskyi@gmail.com>
2022-10-09 16:43:04 +0300

    Initial commit

---
+++ src/test/java/ua/khshanovskyi/LargestMarsPictureWithNasaFeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.khshanovskyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargestMarsPictureWithNasaFeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
